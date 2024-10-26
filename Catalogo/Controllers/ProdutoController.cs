433478fba295d7f3accb754a33fee63b58927c3a
MatheusF3lipe <matheusvld77@gmail.com>
2024-10-25 21:56:40 -0300

    Criação de um novo endpoint para buscar por um id

--- Catalogo/Controllers/ProdutoController.cs
+++ Catalogo/Controllers/ProdutoController.cs
@@ -14,9 +14,19 @@ public class ProdutoController : ControllerBase
         _context = context;
     }
 
-    [HttpGet]
+    [HttpGet("ListarTodosOsProdutos")]
     public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
     {
         return await _context.Produtos.ToListAsync();
+    }
+    [HttpGet("{id}", Name="Listavida")]
+    public async Task<ActionResult<Produto>> ListarProdutoId(int id)
+    {
+        Produto produto = await _context.Produtos.Where(p => p.ProdutoId.Equals(id)).FirstOrDefaultAsync();
+        if (produto is null)
+        {
+            return NotFound("Produto Não localizado...");
+        }
+        return Ok(produto);
     }
 }
